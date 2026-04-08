59702abc40fbc087b7de78af45226a66d7877c46
Panmeii <134782372+Panmeii@users.noreply.github.com>
2026-04-08 12:02:57 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,14 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Inter:wght@400;600&display=swap" 
-          rel="stylesheet" 
-        />
-      </head>
-      <body className="bg-black overflow-hidden">{children}</body>
+      <body className="bg-black overflow-hidden m-0 p-0">{children}</body>
     </html>
   );
 }
